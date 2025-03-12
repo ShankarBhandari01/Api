0c675117b634d143bb4d8c490e2a07c3181281d4
Shankar Bhandari <shankarbhandari@MacBook-Air.local>
2025-03-12 23:29:27 +0000

    adds test end points

--- router/api/index.js
+++ router/api/index.js
@@ -1,12 +1,16 @@
-const router = require('express').Router();
-const user = require('./UserRoute');
-const Stock = require('./stockroute');
-const streamingRoute = require('../api/videoStramingRout');
+const router = require("express").Router();
+const user = require("./UserRoute");
+const Stock = require("./stockroute");
+const streamingRoute = require("../api/videoStramingRout");
 
-//binding of route 
-router.use(Stock)
+//binding of route
+router.use(Stock);
 router.use(user);
 router.use(streamingRoute);
 
+//test url
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
 
 module.exports = router;
