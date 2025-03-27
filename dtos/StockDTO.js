e9d6a8e5b5a0383dbee9631707d4605712b3aecd
Shankar Bhandari <harryshankar@outlook.com>
2025-03-27 15:49:34 +0000

    item perference adds

--- dtos/StockDTO.js
+++ dtos/StockDTO.js
@@ -1,32 +1,35 @@
 class StockDTO {
-    constructor(stock, stockImage) {
-        this.id = stock.id;
-        this.image = stockImage || stock.image;
-        this.stockName = this.parseLangField(stock.stockName);
-        this.description = this.parseLangField(stock.description);
-        this.remarks = this.parseLangField(stock.remarks);
-        this.quantity = stock.quantity || 0;
-        this.amount = stock.amount || 0;
-        this.createdDate = stock.createdDate;
-        this.updatedDate = stock.updated_ts || stock.createdDate;
-        this.mode = stock.mode || "new";
-        this.categoryID = stock.categoryID;
-        this.isDayOfWeek = stock.isDayOfWeek;
-        this.dayOfWeek = stock.dayOfWeek;
-        this.nameOfWeek = this.parseLangField(stock.nameOfWeek)
-    }
+  constructor(stock, stockImage) {
+    this.id = stock.id;
+    this.image = stockImage || stock.image;
+    this.stockName = this.parseLangField(stock.stockName);
+    this.description = this.parseLangField(stock.description);
+    this.remarks = this.parseLangField(stock.remarks);
+    this.quantity = stock.quantity || 0;
+    this.amount = stock.amount || 0;
+    this.createdDate = stock.createdDate;
+    this.updatedDate = stock.updated_ts || stock.createdDate;
+    this.mode = stock.mode || "new";
+    this.categoryID = stock.categoryID;
+    this.isDayOfWeek = stock.isDayOfWeek;
+    this.dayOfWeek = stock.dayOfWeek;
+    this.nameOfWeek = this.parseLangField(stock.nameOfWeek);
+    this.isSpicy = stock.isSpicy;
+    this.isVagen = stock.isVagen;
+    this.isVegetarian = stock.isVegetarian;
+  }
 
-    // stringified JSON fields
-    parseLangField(field) {
-        if (typeof field === "string") {
-            try {
-                field = JSON.parse(field);
-            } catch (e) {
-                field = {};
-            }
-        }
-        return field;
+  // stringified JSON fields
+  parseLangField(field) {
+    if (typeof field === "string") {
+      try {
+        field = JSON.parse(field);
+      } catch (e) {
+        field = {};
+      }
     }
+    return field;
+  }
 }
 
 module.exports = StockDTO;
--- model/Stocks.js
+++ model/Stocks.js
@@ -1,80 +1,85 @@
-const {Schema, model} = require("mongoose");
+const { Schema, model } = require("mongoose");
 //creating the Stock scheme
 const stockSchema = new Schema(
-    {
-        categoryID: {
-            type: Schema.Types.ObjectId,
-            ref: "Category", // Reference to the Category schema
-            required: true,
-        },
-        image: {
-            type: String,
-            required: false,
-        },
-        stockName: {
-            en: {type: String, required: true, trim: true},
-            fi: {type: String, required: false, trim: true},
-        },
-        description: {
-            en: {type: String, required: true},
-            fi: {type: String, required: false},
-        },
-        remarks: {
-            en: {type: String, default: "new listed"},
-            fi: {type: String, default: "uusi listattu"},
-        },
-        quantity: {
-            type: Number,
-            required: true,
-            default: 0,
-            min: 0,
-        },
-        amount: {
-            type: Number,
-            required: true,
-            min: 0,
-        },
-        currency: {type: String, default: "€"},
-        isActive: {type: Boolean, default: true},
-        isDeleted: {type: Boolean, default: false},
-        isDayOfWeek: {type: Boolean, default: false},
-        dayOfWeek: {type: Number, default: 0},
-        nameOfWeek: {
-            en: {type: String, required: true},
-            fi: {type: String, required: false},
-        },
+  {
+    categoryID: {
+      type: Schema.Types.ObjectId,
+      ref: "Category", // Reference to the Category schema
+      required: true,
     },
-    {timestamps: {createdAt: "createdDate", updatedAt: "updated_ts"}}
+    image: {
+      type: String,
+      required: false,
+    },
+    stockName: {
+      en: { type: String, required: true, trim: true },
+      fi: { type: String, required: false, trim: true },
+    },
+    description: {
+      en: { type: String, required: true },
+      fi: { type: String, required: false },
+    },
+    remarks: {
+      en: { type: String, default: "new listed" },
+      fi: { type: String, default: "uusi listattu" },
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 0,
+      min: 0,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    currency: { type: String, default: "€" },
+    isActive: { type: Boolean, default: true },
+    isDeleted: { type: Boolean, default: false },
+    isDayOfWeek: { type: Boolean, default: false },
+    dayOfWeek: { type: Number, default: 0 },
+    nameOfWeek: {
+      en: { type: String, required: true },
+      fi: { type: String, required: false },
+    },
+    isSpicy: { type: Number, require: false },
+    isVagen: { type: Number, require: false },
+    isVegetarian: { type: Number, require: false },
+  },
+  { timestamps: { createdAt: "createdDate", updatedAt: "updated_ts" } }
 );
 
 // Food Category Schema
 const categorySchema = new Schema({
-    name: {
-        en: {type: String, required: true, unique: false, trim: true},
-        fi: {type: String, required: false, unique: false, trim: true},
-    },
-    description: {
-        en: {type: String, required: true, unique: false, trim: true},
-        fi: {type: String, required: false, unique: false, trim: true},
-    },
-    created_at: {
-        type: Date,
-        default: Date.now,
-    },
-    updated_at: {
-        type: Date,
-        default: Date.now,
-    },
-    remarks: {
-        en: {type: String, default: "new listed"},
-        fi: {type: String, default: "uusi listattu"},
-    },
-    isActive: {
-        type: Boolean, default: true
-    },
-    isDeleted: {
-        type: Boolean, default: false
-    }
+  name: {
+    en: { type: String, required: true, unique: false, trim: true },
+    fi: { type: String, required: false, unique: false, trim: true },
+  },
+  description: {
+    en: { type: String, required: true, unique: false, trim: true },
+    fi: { type: String, required: false, unique: false, trim: true },
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
+  remarks: {
+    en: { type: String, default: "new listed" },
+    fi: { type: String, default: "uusi listattu" },
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 // Food Category Model
@@ -82,4 +87,4 @@ const Category = model("Category", categorySchema);
 const Stock = model("Stock", stockSchema);
 
 // Export models for use
-module.exports = {Category, Stock};
+module.exports = { Category, Stock };
