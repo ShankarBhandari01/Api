7c3029be3d0e8c84d5f184da4ee348f84c820a2d
Shankar Bhandari <harryshankar@outlook.com>
2025-03-29 20:38:18 +0000

    image minetype adds

--- config/appconfig.js
+++ config/appconfig.js
@@ -35,6 +35,6 @@ module.exports = {
   },
   file: {
     uploadDir: path.join(__dirname, "../public/images"),
-    ALLOWED_FILE_TYPES: process.env.ALLOWED_FILE_TYPES || ["image/jpeg", "image/png", "image/gif"],
+    ALLOWED_FILE_TYPES: process.env.ALLOWED_FILE_TYPES || ["image/jpeg", "image/png", "image/gif","image/svg"],
   },
 };
