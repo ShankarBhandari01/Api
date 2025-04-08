85a7cbdf6fff49e0955463307c45e65aedcde03b
Shankar Bhandari <harryshankar@outlook.com>
2025-04-08 11:31:51 +0100

    cors policy

--- middleware/CorsMiddleware.js
+++ middleware/CorsMiddleware.js
@@ -1,23 +1,29 @@
 // middlewares/cors.middleware.js
-const cors = require('cors');
+const cors = require("cors");
 
-const whitelist = ['http://localhost:3001','http://127.0.0.1:3000', 'https://restaurantfin.vercel.app/'];
+const whitelist = [
+  "http://localhost:3001",
+  "http://127.0.0.1:3001",
+  "http://localhost:3000",
+  "http://127.0.0.1:3000",
+  "https://restaurantfin.vercel.app/",
+];
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if (process.env.NODE_ENV === 'test') {
+    if (process.env.NODE_ENV === "test") {
       callback(null, true); // Allow all origins in test environment
     } else {
       if (!origin || whitelist.includes(origin)) {
         callback(null, true);
       } else {
-        callback(new Error('Not allowed by CORS'));
+        callback(new Error("Not allowed by CORS"));
       }
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   optionsSuccessStatus: 200,
 };
 
