848ef64b19947b33cb353d0de4a217ab5161641c
Shankar Bhandari <harryshankar@outlook.com>
2025-05-03 19:37:43 +0100

    local replica

--- database/ConnectionManager.js
+++ database/ConnectionManager.js
@@ -57,7 +57,7 @@ class MongoConnectionManager extends logger {
       if (this.env === "development" || this.env === "test") {
         uri = `mongodb+srv://${this.config.username}:${this.config.password}@${this.config.host}/${dbName}?retryWrites=true&w=majority&appName=Cluster0`;
       } else {
-        uri = `mongodb://${this.config.username}:${this.config.password}@127.0.0.1:27017/${dbName}?authSource=admin&replicaSet=rs0&directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.0`;
+        uri = `mongodb://${this.config.username}:${this.config.password}@127.0.0.1:27017/${dbName}?authSource=admin&replicaSet=rs0&directConnection=true`;
       }
       this.pendingConnections[dbName] = this.retryConnection(
         uri,
