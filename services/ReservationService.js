1c9e70f19228748a370d46def9b3eb6a50b2ee06
Shankar Bhandari <harryshankar@outlook.com>
2025-05-25 16:50:14 +0100

    sorting reservation

--- services/ReservationService.js
+++ services/ReservationService.js
@@ -90,7 +90,7 @@ class ReservationService extends BaseService {
       //  Check cache
       const cached = await this.redisSocketService.getCacheValue(cacheKey);
       if (cached) {
-        const sortedDesc = cached.sort(
+        const sortedDesc = cached.data.sort(
           (a, b) => new Date(b.reservation_date) - new Date(a.reservation_date)
         );
         return sortedDesc;
