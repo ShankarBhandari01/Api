698478b3d641d82d43ca38cd444de424cd963035
Shankar Bhandari <harryshankar@outlook.com>
2025-06-13 18:03:45 +0100

    cors allow

--- middleware/CorsMiddleware.js
+++ middleware/CorsMiddleware.js
@@ -21,7 +21,8 @@ export default () => {
     try {
       const isDev =
         process.env.NODE_ENV === "test" ||
-        process.env.NODE_ENV === "development";
+        process.env.NODE_ENV === "development" ||
+        process.env.NODE_ENV === "production";
 
       const adminService = req.scope.resolve("adminService");
 
